f2fa3ada4ed555d30120d8ddebadf629919c622c
Enno Rehling <enno.rehling@gmail.com>
2015-06-13 18:35:08 +0200

    integration tests, starting services. - reorganize travis build - watch out for the ennodb command line arguments.

---
+++ itest/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+ROOT=`pwd`
+[ -n $ITEST_PREFIX ] || ITEST_PREFIX=/tmp/itest
+
+make ennodb
+mkdir -p ${ITEST_PREFIX}/nginx
+#[ -e ${ITEST_PREFIX}/ennodb.socket ] || spawn-fcgi -P ${ITEST_PREFIX}/ennodb.pid -s ${ITEST_PREFIX}/ennodb.socket -- ./ennodb itest/ennodb.ini
+[ -e ${ITEST_PREFIX}/ennodb.socket ] || spawn-fcgi -P ${ITEST_PREFIX}/ennodb.pid -s ${ITEST_PREFIX}/ennodb.socket -- ./ennodb ${ITEST_PREFIX}/binlog
+/usr/sbin/nginx -c $ROOT/itest/nginx.conf -p ${ITEST_PREFIX} -g "pid ${ITEST_PREFIX}/nginx.pid;"
---
+++ itest/teardown.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+[ -n $ITEST_PREFIX ] || ITEST_PREFIX=/tmp/itest
+
+kill_service() {
+if [ -s $1.pid ]; then
+echo "Shutting down $1"
+kill `cat $1.pid`
+fi
+}
+
+if [ -d ${ITEST_PREFIX} ]; then
+cd ${ITEST_PREFIX}
+kill_service nginx
+kill_service ennodb
+cd ..
+rm -rf ${ITEST_PREFIX}
+fi
