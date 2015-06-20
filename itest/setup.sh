68d8e4ef8b6fffe7ae92afe9f10eace8b108196b
Enno Rehling <enno.rehling@gmail.com>
2015-06-20 09:41:19 +0200

    add an integration test for single-line results, check string. fails.

--- itest/setup.sh
+++ itest/setup.sh
@@ -8,3 +8,8 @@ make ennodb
 mkdir -p ${ITEST_PREFIX}/nginx
 [ -e ${ITEST_PREFIX}/ennodb.socket ] || spawn-fcgi -P ${ITEST_PREFIX}/ennodb.pid -s ${ITEST_PREFIX}/ennodb.socket -- ./ennodb itest/ennodb.ini
 /usr/sbin/nginx -c $ROOT/itest/nginx.conf -p ${ITEST_PREFIX} -g "pid ${ITEST_PREFIX}/nginx.pid;"
+
+if [ "$1" = "-e" ]; then
+export ITEST_HOST=localhost
+export ITEST_PORT=8080
+fi
